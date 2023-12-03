fad378f6ee2c5086f12feeabe31210dd68dad6ac
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-12-03 21:21:08 +0700

    add aplikasiSiswa extension function to PenyediaViewModel object

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,3 +1,17 @@
 package com.example.roomsiswa.model
 
-object PenyediaViewModel{}
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel{
+    /**
+     * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa
+     */
+    fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
+}
+
