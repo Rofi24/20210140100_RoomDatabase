99d07a615e79c71991657b17625b533883f86aad
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-12-04 15:45:37 +0700

    add DataSiswa component with Card

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -1,7 +1,10 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.roomsiswa.AplikasiSiswa
 import com.example.roomsiswa.R
 import com.example.roomsiswa.data.Siswa
@@ -16,4 +19,11 @@ object DestinasiHome : DestinasiNavigasi{
 fun DataSiswa(
     siswa: Siswa,
     modifier: Modifier = Modifier
-){}
+){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+
+    }
+}
