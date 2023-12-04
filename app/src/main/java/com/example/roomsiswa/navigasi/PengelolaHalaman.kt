b19e656a57aaa4fc72b294939dd82d5c1026777a
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-12-04 17:22:21 +0700

    add NavHost with DestinasiHome and DestinasiEntry composables

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -13,7 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.halaman.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,4 +50,15 @@ fun SiswaTopAppBar(
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-){}
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigetBack = {navController.popBackStack()})
+        }
+    }
+}
