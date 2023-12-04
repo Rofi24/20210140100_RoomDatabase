ce3d0305140ae8e517c8cd79f2adda3b853b742d
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-12-04 16:54:59 +0700

    add CenterAlignedTopAppBar component

--- app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -1,10 +1,17 @@
 package com.example.roomsiswa.navigasi
 
-import android.icu.text.CaseMap.Title
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.roomsiswa.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,4 +21,19 @@ fun SiswaTopAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior?  = null,
     navigateUp: () -> Unit = {}
-){}
+){
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
